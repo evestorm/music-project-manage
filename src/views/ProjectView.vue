52a20727ba70491bbcd92d46a4857e7538498cdd
杨亮 <1572534835@qq.com>
2023-02-25 16:55:01 +0800

    group-table

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -2,7 +2,8 @@
   <!--<Demo />-->
   <!--<Toolbar />-->
   <!--<VirtualDOMList />-->
-  <Config />
+  <!--<Config />-->
+  <GroupTable />
 </template>
 
 <script setup>
@@ -10,6 +11,7 @@ import Demo from '@/components/basic/demo.vue';
 import Toolbar from '@/components/basic/toolbar.vue';
 import VirtualDOMList from "@/components/basic/virtualDOMList.vue";
 import Config from '@/components/basic/config.vue';
+import GroupTable from '@/components/basic/groupTable.vue';
 </script>
 
 <style scoped lang="scss"></style>
