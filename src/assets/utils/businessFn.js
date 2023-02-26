a0817d44dcfc9d1c4af17e9969fd891cc896383e
杨亮 <1572534835@qq.com>
2023-02-26 22:22:14 +0800

    update common.scss

--- src/assets/utils/businessFn.js
+++ src/assets/utils/businessFn.js
@@ -1,20 +1,30 @@
 // 合并项目
+// resource: https://vxetable.cn/#/table/api?filterName=span-method
 export function mergeRowMethod({ row, _rowIndex, column, visibleData }) {
-	const fields = ['projectId'];
-	const cellValue = row[column.field];
-	if (cellValue && fields.includes(column.field)) {
-		const prevRow = visibleData[_rowIndex - 1];
-		let nextRow = visibleData[_rowIndex + 1];
-		if (prevRow && prevRow[column.field] === cellValue) {
-			return { rowspan: 0, colspan: 0 };
-		} else {
-			let countRowspan = 1;
-			while (nextRow && nextRow[column.field] === cellValue) {
-				nextRow = visibleData[++countRowspan + _rowIndex];
-			}
-			if (countRowspan > 1) {
-				return { rowspan: countRowspan, colspan: 1 };
-			}
-		}
-	}
+  console.log({
+    row,
+    _rowIndex,
+    column,
+    visibleData,
+  });
+  // 只有前两列的 column.field 才等于 projectId 或 stageId；其他情况下是 timestamp
+  const fields = ["projectId"];
+  const cellValue = row[column.field];
+  if (cellValue && fields.includes(column.field)) {
+    const prevRow = visibleData[_rowIndex - 1]; // 前一行
+    let nextRow = visibleData[_rowIndex + 1]; // 后一行
+    if (prevRow && prevRow[column.field] === cellValue) {
+      // 起始
+      return { rowspan: 0, colspan: 0 };
+    } else {
+      let countRowspan = 1;
+      while (nextRow && nextRow[column.field] === cellValue) {
+        nextRow = visibleData[++countRowspan + _rowIndex];
+      }
+      if (countRowspan > 1) {
+        // 终止
+        return { rowspan: countRowspan, colspan: 1 };
+      }
+    }
+  }
 }
--- src/components/TableMain/js/utils.js
+++ src/components/TableMain/js/utils.js
@@ -1,12 +1,12 @@
-import { formatDate } from "../../../utils/date";
+import { formatDate } from "@/utils/date";
 
 // 设置表头 今天 高亮
 export function setHeaderCell({ $rowIndex, column, columnIndex, $columnIndex }) {
-    // console.log({ $rowIndex, column, columnIndex, $columnIndex });
-    const { field } = column;
-    // 今日时间戳
-    const nowTimeStamp = new Date(formatDate(new Date().getTime())).getTime();
-    if(field == nowTimeStamp) {
-        return 'now-day-header';
-    }
+  // console.log({ $rowIndex, column, columnIndex, $columnIndex });
+  const { field } = column;
+  // 今日时间戳
+  const nowTimeStamp = new Date(formatDate(new Date().getTime())).getTime();
+  if (field === nowTimeStamp) {
+    return "now-day-header";
+  }
 }
